4863cfb4fde9250e4a8f2dd886e7bd9c847989c3
VictordaSiIva <40555247+VictordaSiIva@users.noreply.github.com>
2018-10-26 21:50:40 -0300

    Add files via upload
    teste

---
+++ Exercicios/Recovered-set-20-2018-0831.Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, resto;
+
+          
+
+
+
+            // exercicio 1
+
+            Console.WriteLine("***** Exercicio 1 *****");
+
+            Console.WriteLine("informe um numero");
+            num1 = double.Parse(Console.ReadLine());
+
+           
+
+            if (num1 > 0)
+            {
+                Console.WriteLine("este numero é positivo");
+            }
+            else if (num1 == 0)
+            {
+                Console.WriteLine("Este numero é igual a 0");
+            }
+
+            else
+            {
+                Console.WriteLine("Este numero é negativo");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("pressione uma tecla para prosseguir");
+            Console.ReadLine();
+
+            // exercicio 2
+
+            Console.WriteLine("***** Exercicio 2 *****");
+           
+            Console.WriteLine("Informe um numero");
+            num2 = double.Parse(Console.ReadLine());
+
+            resto = num2 % 2;
+
+            if (resto == 0)
+            {
+                Console.WriteLine("Este numero é par");
+            }
+            else
+            {
+                Console.WriteLine("Este numero é Impar");
+            }
+
+            Console.Read();
+
+
+        }
+    }
+}
---
+++ ExerciciosSwicthCAse/Recovered-out-19-2018-0713.Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double op, n1, n2, resultado;
+
+            Console.WriteLine("Calculadora basica");
+            Console.WriteLine("informe a operação \n 1- Adição \n 2- subtração \n 3- multiplicação \n 4- divisão");
+            op = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("INforme o primeiro numero");
+            n1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("INforme o segundo numero");
+            n2 = double.Parse(Console.ReadLine());
+
+          
+                switch (op)
+                {
+                    case 1:
+
+                        resultado = n1 + n2;
+
+                        Console.WriteLine(" O resultado é: " + resultado);
+                        break;
+
+                    case 2:
+
+                        resultado = n1 - n2;
+
+                        Console.WriteLine(" O resultado é: " + resultado);
+                        break;
+
+                    case 3:
+
+                        resultado = n1 * n2;
+
+                        Console.WriteLine(" O resultado é: " + resultado);
+                        break;
+
+                    case 4:
+
+
+
+                        if (n2 == 0)
+                        {
+                            Console.WriteLine("Impossivel dividir por 0");
+
+                        }
+                        else
+                        {
+                            resultado = n1 / n2;
+                            Console.WriteLine(" O resultado é: " + resultado);
+                        }
+
+
+                        break;
+
+
+
+                }
+            
+
+            Console.Read();
+        }
+    }
+}
